58209b69532688654ca59010940c1653e3f6b50e
Yanney677 <w.yanney1991@gmail.com>
2023-10-03 20:35:13 -0700

    Regular Expressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,4 +3,4 @@
 # The sender phone number or name (including country code if present)
 # The receiver phone number or name (including country code if present)
 # The flags that were used in a textme message.
-ARGF.read.scan(/Message from ([^,]+), to ([^,]+), Flags: ([^\n]+)/) { |m| puts "#{m[0]},#{m[1]},#{m[2]}" }
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
