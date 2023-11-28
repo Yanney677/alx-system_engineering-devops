e0b2307b0ef359f6868e10de81ef3d5ae9f0b0ba
Yanney677 <w.yanney1991@gmail.com>
2023-11-27 18:02:06 -0800

    Client configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,11 +2,11 @@
 # Using puppet to make changes to our configuration file
 
 file { '/etc/ssh/ssh_config':
-	ensure => present,
-content        => "
-	#SSH client configuration
-	Host *
-	IdentitiFile ~/.ssh/school
-	PasswordAuthentication no
-	" ,
+  ensure  => present,
+content   => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
 }
