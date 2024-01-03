f46479852f6be54e2a11f880561e28ea7d6f53a6
Yanney677 <w.yanney1991@gmail.com>
2024-01-03 07:41:50 +0000

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """Export all employees information to JSON format."""
 import json
-import requests as requests
+import requests
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
-
 
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
@@ -14,6 +13,5 @@ if __name__ == "__main__":
                 "task": t.get("title"),
                 "completed": t.get("completed"),
                 "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+            } for t in requests.get(url + "todos", params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile, indent=2)
