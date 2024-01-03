a2207ddd345a23adbca967a6465ee4c6b39a03fe
Yanney677 <w.yanney1991@gmail.com>
2024-01-03 07:15:33 +0000

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Export employee ID to CSV format."""
-import sys
-import requests as requests
 import csv
+import requests as requests
+import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
