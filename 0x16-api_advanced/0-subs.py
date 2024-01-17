85350f6392e2d99b00e14e725cbcaad74e5957d5
Yanney677 <w.yanney1991@gmail.com>
2024-01-17 03:20:48 +0000

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,17 +3,18 @@
 (not active users, total subscribers) for a given subreddit
 """
 import requests
-import sys
 
 def number_of_subscribers(subreddit):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
         'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
     }
-    resp = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
-        subreddit), headers=headers, allow_redirects=False)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    resp = requests.get(url, headers=headers, allow_redirects=False)
+    
     if resp.status_code >= 300:
         return 0
-    json = resp.json()
-    data_dict = json.get('data')
-    return(data_dict.get('subscribers'))
+    
+    json_data = resp.json()
+    data_dict = json_data.get('data')
+    return data_dict.get('subscribers')
