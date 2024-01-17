d07be36f47de7ee06ef718d64db8442c9f78a635
Yanney677 <w.yanney1991@gmail.com>
2024-01-17 12:22:09 +0000

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#Replaces typo string in wp settings file
+exec { 'change to php':
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-setti\
+ngs.php",
+  path    => '/bin'
+}
