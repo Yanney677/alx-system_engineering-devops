6e7173364d0e7a04e73f54d77a3dae71804951a6
Yanney677 <w.yanney1991@gmail.com>
2024-01-17 03:06:14 +0000

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
-
+"""Queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit
+"""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    Return the total number of subscribers on a given subreddit.
-
-    Parameters:
-    - subreddit (str): The name of the subreddit.
-
-    Returns:
-    - int: The total number of subscribers. Returns 0 if the subreddit is not found.
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise an error for bad responses (e.g., 404)
-
-        if response.status_code == 404:
-            return 0
-
-        results = response.json().get("data")
-        return results.get("subscribers", 0)
-
-    except requests.exceptions.RequestException as e:
-        print("An error occurred: {}".format(e))
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
+        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    }
+    resp = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
+        subreddit), headers=headers, allow_redirects=False)
+    if resp.status_code >= 300:
         return 0
+    json = resp.json()
+    data_dict = json.get('data')
+    return(data_dict.get('subscribers'))
