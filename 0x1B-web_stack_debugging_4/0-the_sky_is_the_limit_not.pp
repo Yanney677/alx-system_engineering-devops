1dda033f4c2f97d46161e26d9c7b08f82559cd7b
Yanney677 <w.yanney1991@gmail.com>
2024-01-28 03:01:44 +0000

    Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # A scripts that fix the number of failed requests to get to 0
-exec { 'fix--for-nginx':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+exec { 'change ULIMIT':
+  command => "sed -i 's/15/4096/g' /etc/default/nginx ; service nginx restart",
   path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
